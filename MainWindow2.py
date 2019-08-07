030d499b7d0c03824dbeb08898edca65ebdd81e7
Riccardo Grespan <vxrich@localhost.localdomain>
2019-08-07 22:42:44 +0200

    Working on variables' name

--- MainWindow2.py
+++ MainWindow2.py
@@ -22,12 +22,10 @@ class MainWindow(QMainWindow):
         
         #dim = height*0.2
         dim = (height-bar_height)*0.11
+        icon_dim = dim*0.6
         space = (height-bar_height)*0.035
         btnCenter = (width-dim)/2
-        
-        #space = 0
-        #print (width, height)
-        #print (dim, space)        
+
 
         self.widget = QWidget(self)
         self.widget.setGeometry(QRect(0, bar_height, width, height-bar_height-dim))
@@ -37,7 +35,7 @@ class MainWindow(QMainWindow):
         #self.widget.setStyleSheet("background-color: white; border-radius: 10px;")
         self.widget.setStyleSheet("background-color: #71787a; border-radius: 10px;")
 
-        #BARS
+        #-----------BARS-----------
         self.bar = QWidget(self)
         self.bar.setGeometry(QRect(0,0, width, bar_height))
         self.bar.setStyleSheet("background-color: black;")
@@ -46,7 +44,7 @@ class MainWindow(QMainWindow):
         self.bottomBar.setGeometry(QRect(0,height-dim, width, dim))
         self.bottomBar.setStyleSheet("background-color: black;")
 
-        #DATA
+        #-----------DATA-----------
         self.date_time = QLabel(self.bar)
         self.date_time.setGeometry(QRect((width-width*0.3)/2, 0,width*0.3, bar_height))
         self.date_time.setAlignment(Qt.AlignCenter)
@@ -58,25 +56,25 @@ class MainWindow(QMainWindow):
         self.timer.timeout.connect(self.updateTime)
         self.timer.start(1000)
         
-        #BUTTONS
+        #-----------BUTTONS-----------
 
-        self.shutdownBtn = QPushButton(QIcon("src/shutdown.png"), "", self.bottomBar)
+        self.shutdownBtn = QPushButton(QIcon("src/menu/shutdown.png"), "", self.bottomBar)
         self.shutdownBtn.setGeometry(QRect(0, 0, dim, dim))
-        self.shutdownBtn.setIconSize(QSize(dim, dim))
+        self.shutdownBtn.setIconSize(QSize(icon_dim, icon_dim))
         self.shutdownBtn.setObjectName("shutdown")
         self.shutdownBtn.setStyleSheet("background-color: black; border-radius: 10px;")
         self.shutdownBtn.clicked.connect(self.onClickShutdown)
 
-        self.homeBtn = QPushButton(QIcon("src/home-pressed.png"), "", self.bottomBar)
+        self.homeBtn = QPushButton(QIcon("src/menu/home-pressed.png"), "", self.bottomBar)
         self.homeBtn.setGeometry(QRect((width-dim)/2-2*dim, 0, dim, dim))
-        self.homeBtn.setIconSize(QSize(dim, dim))
+        self.homeBtn.setIconSize(QSize(icon_dim, icon_dim))
         self.homeBtn.setObjectName("home")
         self.homeBtn.setStyleSheet("background-color: black; border-radius: 10px;")
         self.homeBtn.clicked.connect(self.onClickHome)
 
-        self.musicBtn = QPushButton(QIcon("src/music.png"), "", self.bottomBar)
+        self.musicBtn = QPushButton(QIcon("src/menu/music.png"), "", self.bottomBar)
         self.musicBtn.setGeometry(QRect((width-dim)/2-dim, 0, dim, dim))
-        self.musicBtn.setIconSize(QSize(dim, dim))
+        self.musicBtn.setIconSize(QSize(icon_dim, icon_dim))
         self.musicBtn.setObjectName("music")
         self.musicBtn.setStyleSheet("background-color: black; border-radius: 10px;")
         self.musicBtn.clicked.connect(self.onClickMusic)
@@ -87,16 +85,16 @@ class MainWindow(QMainWindow):
         self.temperatureBtn.setStyleSheet("background-color: grey;")
         self.temperatureBtn.setStyleSheet("font-size:" + str(int(dim/3)) + "px;")
 
-        self.notesBtn = QPushButton(QIcon("src/notes.png"), "", self.bottomBar)
+        self.notesBtn = QPushButton(QIcon("src/menu/notes.png"), "", self.bottomBar)
         self.notesBtn.setGeometry(QRect((width-dim)/2+dim, 0, dim, dim))
-        self.notesBtn.setIconSize(QSize(dim,dim))
+        self.notesBtn.setIconSize(QSize(icon_dim,icon_dim))
         self.notesBtn.setObjectName("notes")
         self.notesBtn.setStyleSheet("background-color: black; border-radius: 10px;")
         self.notesBtn.clicked.connect(self.onClickNotes)
 
-        self.settingsBtn = QPushButton(QIcon("src/settings.png"), "", self.bottomBar)
+        self.settingsBtn = QPushButton(QIcon("src/menu/settings.png"), "", self.bottomBar)
         self.settingsBtn.setGeometry(QRect((width-dim)/2+2*dim, 0, dim, dim))
-        self.settingsBtn.setIconSize(QSize(dim, dim))
+        self.settingsBtn.setIconSize(QSize(icon_dim, icon_dim))
         self.settingsBtn.setObjectName("settings")
         self.settingsBtn.setStyleSheet("background-color: black ;border-radius: 10px;")
         self.settingsBtn.clicked.connect(self.onClickSettings)
@@ -123,7 +121,7 @@ class MainWindow(QMainWindow):
 
         self.changeTab()
 
-        self.homeBtn.setIcon(QIcon("src/home-pressed.png"))
+        self.homeBtn.setIcon(QIcon("src/menu/home-pressed.png"))
 
         if self.home == None:
             self.home = Home(self.widget)
@@ -137,7 +135,7 @@ class MainWindow(QMainWindow):
         
         self.changeTab()
 
-        self.musicBtn.setIcon(QIcon("src/music-pressed.png"))
+        self.musicBtn.setIcon(QIcon("src/menu/music-pressed.png"))
 
         if self.music == None:
             self.music = Music(self.widget)
@@ -154,7 +152,7 @@ class MainWindow(QMainWindow):
 
         self.changeTab()
 
-        self.notesBtn.setIcon(QIcon("src/notes-pressed.png"))
+        self.notesBtn.setIcon(QIcon("src/menu/notes-pressed.png"))
         """
         if self.notes == None:
             self.notes = Notes(self.widget)
@@ -169,7 +167,7 @@ class MainWindow(QMainWindow):
 
         self.changeTab()
 
-        self.settingsBtn.setIcon(QIcon("src/settings-pressed.png"))
+        self.settingsBtn.setIcon(QIcon("src/menu/settings-pressed.png"))
         """
         if self.settings == None:
             self.settings = Settings(self.widget)
@@ -182,14 +180,14 @@ class MainWindow(QMainWindow):
 
     def onClickShutdown(self):
 
-        self.shutdownBtn.setIcon(QIcon("src/shutdown-pressed.png"))
+        self.shutdownBtn.setIcon(QIcon("src/menu/shutdown-pressed.png"))
 
         os.system("shutdown now")
 
     def changeTab(self):
 
         self.current.hide()
-        btnIconPath = "src/" + self.currentBtn.objectName() + ".png"
+        btnIconPath = "src/menu/" + self.currentBtn.objectName() + ".png"
         self.currentBtn.setIcon(QIcon(btnIconPath))        
 
 
